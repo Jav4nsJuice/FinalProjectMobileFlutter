d520ce0790682b8e93707b8afab231f6fd7efed1
Andres <53712714+Andres-Pereira@users.noreply.github.com>
2023-05-30 17:57:17 -0400

    Notifications implemented with FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_mobile_final/api_manager.dart';
+import 'package:flutter_app_mobile_final/firebase_options.dart';
 import 'package:flutter_app_mobile_final/libertadores_page.dart';
 import 'package:flutter_app_mobile_final/ligue_main.dart';
 import 'package:flutter_app_mobile_final/page_body.dart';
 
 import 'item_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
